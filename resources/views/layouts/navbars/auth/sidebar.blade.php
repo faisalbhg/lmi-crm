705e7b51e19d06a9f549a2f19ea724e470e7f004
Faisal ck <faisal@buhaleeba.ae>
2023-08-23 10:13:39 +0400

    feedback 23-08-2023 10:13am

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -43,7 +43,7 @@
                 </a>
             </li>
             @endif
-            @if(Session::get('user')->isadmin || Session::get('user')->usertype == 7  || Session::get('user')->feedbackSettings)
+            @if(Session::get('user')->isadmin  || Session::get('user')->feedbackSettings)
             <li class="nav-item mt-2">
                 <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Feedback</h6>
             </li>
